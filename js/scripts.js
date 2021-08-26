6e43772a4ae2ecefbb7133b590077724b0d2e5de
Bruno Almeida <bruno.ag@sempreceub.com>
2021-08-26 15:11:31 -0300

    commit

--- js/scripts.js
+++ js/scripts.js
@@ -105,6 +105,7 @@ $( document ).ready(function() {
   
     // setTimeout serve para carregar primeiro as imagens
     setTimeout(function() {
+      $('#data-area').parallax({imageSrc: 'img/cidadeparallax.png'});
       $('#apply-area').parallax({imageSrc: 'img/pattern.png'});
     }, 200);
   
